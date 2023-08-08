e6ed2076e254eeb5ed1a4fb3a0472c84a5b2706e
Isha Singh <isha080400@gmail.com>
2023-08-09 01:28:26 +0530

    [Isha] Add: tests for logging in for UserService

--- demo/src/test/java/com/blogging/demo/service/UserServiceTest.java
+++ demo/src/test/java/com/blogging/demo/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.blogging.demo.service;
 
 import com.blogging.demo.entities.User;
+import com.blogging.demo.exception.InvalidCredentialsException;
+import com.blogging.demo.exception.UserDoesNotExistException;
 import com.blogging.demo.exception.UserExistsException;
 import com.blogging.demo.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -42,5 +47,36 @@ public class UserServiceTest {
         assertThrows(UserExistsException.class,
                 () -> userService.register(email, userName, password),
                 "User with this email already exists!");
+    }
+
+    @Test
+    void shouldLoginWhenRegisteredUserProvidesCorrectCredentials() {
+        String email = "abc@xyz.com";
+        String password = "strongPassword";
+        User user = mock(User.class);
+        when(userRepository.findById(email)).thenReturn(Optional.of(user));
+        when(user.getPassword()).thenReturn(password);
+
+        assertDoesNotThrow(() ->  userService.login(email, password));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUnregisteredUserTriesToLogIn() {
+        String email = "abc@xyz.com";
+        String password = "strongPassword";
+        when(userRepository.findById(email)).thenReturn(Optional.empty());
+
+        assertThrows(UserDoesNotExistException.class, () ->  userService.login(email, password));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenRegisteredUserTriesToLogInWithWrongCredentials() {
+        String email = "abc@xyz.com";
+        String password = "strongPassword";
+        User user = mock(User.class);
+        when(userRepository.findById(email)).thenReturn(Optional.of(user));
+        when(user.getPassword()).thenReturn("differentPassword");
+
+        assertThrows(InvalidCredentialsException.class, () ->  userService.login(email, password));
     }
 }
